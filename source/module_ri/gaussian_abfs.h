cec99e3d0af2ad67075245a493f80732ca4fef92
jiyuang <jiyuyang@mail.ustc.edu.cn>
2024-06-12 17:50:46 +0800

    fix format error

--- source/module_ri/gaussian_abfs.h
+++ source/module_ri/gaussian_abfs.h
@@ -18,7 +18,7 @@
 class Gaussian_Abfs
 {
   public:
-    std::pair<RI::Tensor<std::complex<double>>, std::array<RI::Tensor<std::complex<double>>>, 3>> get_Vq(
+    std::pair<RI::Tensor<std::complex<double>>, std::array<RI::Tensor<std::complex<double>>, 3>> get_Vq(
         const int& lp_max,
         const int& lq_max, // Maximum L for which to calculate interaction.
         const ModuleBase::Vector3<double>& qvec,
@@ -27,22 +27,22 @@ class Gaussian_Abfs
         const double& lambda,
         const ModuleBase::Vector3<double>& tau,
         const ORB_gaunt_table& MGT,
-                        const bool& cal_deriv);
+        const bool& cal_deriv);
 
     /*
 Calculate the lattice sum over a Gaussian:
   S(k) := \sum_G |k+G|^{power+L} \exp(-lambda*|k+G|^2) Y_{LM}(k+G) * \exp(i(k+G)\tau)
   d_S(k) := S(k) * i * (k+G)
 */
-    static std::pair<std::vector<std::complex<double>>, std::vector<std::vector<std::complex<double>>>>
-        get_lattice_sum(const ModuleBase::Vector3<double>& qvec,
-                        const ModuleBase::Matrix3& G,
-                        const double& power, // Will be 0. for straight GTOs and -2. for Coulomb interaction
-                        const double& exponent,
-                        const bool& exclude_Gamma, // The R==0. can be excluded by this flag.
-                        const int& lmax,           // Maximum angular momentum the sum is needed for.
-                        const ModuleBase::Vector3<double>& tau,
-                        const bool& cal_deriv);
+    static std::pair<std::vector<std::complex<double>>, std::vector<std::vector<std::complex<double>>>> get_lattice_sum(
+        const ModuleBase::Vector3<double>& qvec,
+        const ModuleBase::Matrix3& G,
+        const double& power, // Will be 0. for straight GTOs and -2. for Coulomb interaction
+        const double& exponent,
+        const bool& exclude_Gamma, // The R==0. can be excluded by this flag.
+        const int& lmax,           // Maximum angular momentum the sum is needed for.
+        const ModuleBase::Vector3<double>& tau,
+        const bool& cal_deriv);
 
     // construct gaussian basis based on original NAO
     Numerical_Orbital_Lm Gauss(const Numerical_Orbital_Lm& orb, const double& lambda);
